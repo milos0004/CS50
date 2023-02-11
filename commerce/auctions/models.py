6263d4ea56be709fc2b2f2f4046e17fe661e6864
milos0004 <milos.martinovic2000@gmail.com>
2023-02-11 16:21:19 +0000

    updating models py file

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    listingTitle = models.CharField(max_length=64)
+    listingDescription = models.CharField(max_length=512)
+    startBid = models.IntegerField()
+    imageURL = models.URLField()
+    listingCategory = models.CharField(max_length=64)
+
+
+    pass
+
+class Bid(models.Model):
+    currentBid = models.IntegerField()
+    bidIncrement = models.IntegerField()
+
+    pass
+
+class Comment(models.Model):
+    pass

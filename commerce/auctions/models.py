a8dc587a1b29c34fb9d78b96da806bf14044edf6
milos0004 <60904196+milos0004@users.noreply.github.com>
2023-02-17 12:39:51 +0000

    testing models and testing the use of vc codes git

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+Categories = (
+    ('home','Home'), ('technology','Technology'), ('garden','Garden'),('appliances','Appliances'), 
+    ('toys','Toys'),('clothing','Clothing'),('sports','Sports'),('health','Health'),('other','Other')
+)
 class User(AbstractUser):
     pass
 
@@ -10,8 +13,8 @@ class Listing(models.Model):
     listingDescription = models.CharField(max_length=512)
     startBid = models.IntegerField()
     imageURL = models.URLField()
-    listingCategory = models.CharField(max_length=64)
-
+    listingCategory = models.CharField(max_length=10, choices=Categories)
+    listingCreator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     pass
 

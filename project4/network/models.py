8a6b2c30a6798bce9d987440c47ed8134dcce62a
milos0004 <60904196+milos0004@users.noreply.github.com>
2023-04-18 20:04:28 +0100

    updating

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    datetime = models.DateTimeField()
+
+    pass
+
+class Likes(models.Model):
+    pass
+
+class Followers(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    followers = models.ForeignKey(User,on_delete=models.CASCADE)
+    pass

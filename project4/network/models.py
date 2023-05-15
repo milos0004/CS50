0325b99a9946917e1e4d5e87eda66745a2dac720
milos0004 <60904196+milos0004@users.noreply.github.com>
2023-05-15 19:19:46 +0100

    added some tests for models.py

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,5 +1,7 @@
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from datetime import datetime
 
 
 class User(AbstractUser):
@@ -8,15 +10,15 @@ class User(AbstractUser):
 class Post(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=512)
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(default=datetime.now())
 
 
 class Likes(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    liked_at = models.DateTimeField()
+    liked_at = models.DateTimeField(default=datetime.now())
   
 class Followers(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='users_followers')
     followers = models.ForeignKey(User,on_delete=models.CASCADE, related_name="user_following")
-    follow_time = models.DateTimeField()
+    follow_time = models.DateTimeField(default=datetime.now())
--- project4/network/tests.py
+++ project4/network/tests.py
@@ -1,5 +1,35 @@
+
 from django.test import TestCase
 from .models import User, Likes, Post, Followers
 # Create your tests here.
 class PostTestCase(TestCase):
-    u1 =  User.objects.create()
+    def setUp(self):
+        u1 =  User.objects.create(username="milos", password="hi", email="milos@gmail.com")
+        u2 =  User.objects.create(username="andy", password="hi", email="andy@gmail.com")
+        p1 = Post.objects.create(creator=u1, content="first post!!! Hi")
+        l1 = Likes.objects.create(user=u2,post=p1)
+        f1 = Followers.objects.create(user= u1, followers= u2)
+
+    def test_post_creation(self):
+        u1 = User.objects.get(username="milos")
+        u2 = User.objects.get(username="andy")
+        u1p = Post.objects.get(creator=u1)
+        self.assertEquals(u1p.content, "first post!!! Hi")
+        self.assertNotEqual(u1p.creator, u2)
+        self.assertNotEqual(u1p.creator,None)
+
+    
+    def test_likes(self):
+        u1 = User.objects.get(username="milos")
+        u2 = User.objects.get(username="andy")
+        u1p = Post.objects.get(creator=u1)
+        u2l = Likes.objects.get(user=u2)
+        self.assertEquals(u2l.post, u1p)
+        self.assertEquals(u2l.user, u2)
+        
+    def test_Followers(self):
+        u1 = User.objects.get(username="milos")
+        u2 = User.objects.get(username="andy")
+        u1f = Followers.objects.get(user=u1)
+        self.assertEquals(u1f.followers, u2)
+        self.assertEquals(u1f.user, u1)
